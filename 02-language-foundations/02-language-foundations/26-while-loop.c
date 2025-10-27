5bc611c03e3b398ea82bd3b5d11bf3059a47ee26
CollantesAI <miguel.a.collantes@outlook.com>
2025-10-27 14:17:06 -0400

    refactor(02-language-foundations\26-while-loop): improve readability

--- 02-language-foundations/02-language-foundations/26-while-loop.c
+++ 02-language-foundations/02-language-foundations/26-while-loop.c
@@ -6,17 +6,16 @@
 
 int main(void) {
 	int diceValue = 0;
+	int rolls = 0;
 
 	scanf_s(" %d", &diceValue);
-
-	int rolls = 1;
 
 	while (diceValue != 6) {
 		scanf_s(" %d", &diceValue);
 		rolls++;
 	}
 
-	printf("Took %d rolls to get a 6\n", rolls);
+	printf("Took %d rolls to get a 6\n", rolls + 1);
 
 	return 0;
 }
